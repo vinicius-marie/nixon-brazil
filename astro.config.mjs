8977977346d3b100de495958e703edbc4fba459c
Vinícius Daniel <242110551+viniciusdaniel-law@users.noreply.github.com>
2026-07-24 05:19:16 -0300

    Update Pages host after GitHub username change

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ const repository = 'nixon-brazil';
 const customDomain = process.env.CUSTOM_DOMAIN;
 
 export default defineConfig({
-  site: customDomain ? `https://${customDomain}` : 'https://vinicius-marie.github.io',
+  site: customDomain ? `https://${customDomain}` : 'https://viniciusdaniel-law.github.io',
   base: customDomain ? '/' : `/${repository}`,
   trailingSlash: 'always',
   output: 'static',
